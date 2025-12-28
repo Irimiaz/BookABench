60ab8ee4260c17b5a2e2768e7912c838809ca1ca
Marian Irimia <mariani@advahoo.local>
2025-12-28 20:30:26 +0200

    Added explicit loggers

--- Backend/database-service/src/middleware/logger.ts
+++ Backend/database-service/src/middleware/logger.ts
@@ -9,7 +9,7 @@ export const requestLogger = (
 
   res.on("finish", () => {
     const duration = Date.now() - start;
-    const message = `${req.method} ${req.originalUrl} ${res.statusCode} - ${duration}ms`;
+    const message = `${req.method} ${req.originalUrl} ${req.body.api} ${res.statusCode} - ${duration}ms`;
 
     if (res.statusCode >= 400) {
       console.error(message);
