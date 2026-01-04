5852d2069d32ca7ff991d93960b4a9f8b34a9cff
IonitaElena <ionitaelena557@gmail.com>
2026-01-04 16:28:00 +0200

    front

--- Frontend/app/navigation/MainStack.tsx
+++ Frontend/app/navigation/MainStack.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Dashboard from "../dashboard/Dashboard";
 import Products from "../dashboard/Products";
+import Benches from "../dashboard/Benches";
+import Reservations from "../dashboard/Reservations";
 
 const Stack = createNativeStackNavigator();
 
@@ -18,6 +20,8 @@ const MainStack = () => {
     >
       <Stack.Screen name="Dashboard" component={Dashboard} />
       <Stack.Screen name="Products" component={Products} />
+      <Stack.Screen name="Benches" component={Benches} />
+      <Stack.Screen name="Reservations" component={Reservations} />
     </Stack.Navigator>
   );
 };
